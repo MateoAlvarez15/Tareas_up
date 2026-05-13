38c70dd05b60a3658e95baf655919f54a972d9f8
Mateo Alvarez <15mateoalvarrieta@gmail.com>
2026-05-12 22:04:30 -0500

    fix prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,12 @@
-import { PrismaClient } from '../lib/generated/prisma'
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['error'],
-  })
+  globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
